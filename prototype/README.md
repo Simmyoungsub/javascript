3d0224a2ffbd22b0b899316eb13e520ddc539d24
myoungsubsim <rop2001@gmail.com>
2019-07-29 23:19:41 +0900

    prototype 추가

---
+++ prototype/README.md
@@ -0,0 +1,38 @@
+# 프로토 타입
+## 프로토 타입
+- 자바스크립트는 기존의 객체를 복사해서 새로운 객체를 생성하는 프로토타입 기반의 언어
+
+## 프로토타입 객체
+- 함수가 정의될때 기본적으로 Constructor가 부여됨
+- constructor를 가지면 new 연산자를 사용할수있음
+- constructor, __proto__를 가지고 있음
+- constructor는 정의된 함수를 가리키고 있다.
+- new 연산자로 생성된 객체의 __proto__ 정의된 함수의 prototype Object를 가리킨다.
+
+![prototype](./images/prototype.png)
+## 프로토타입 링크
+- __proto__ 
+
+## 프로토타입 체이닝
+- __proto__ 속성을 통해 상위 프로토타입과 연결되어있는 형태
+- 탐색시 __proto__가 null이면 탐색을 종료한다.
+
+## 프로토타입 상속
+- new 생성자
+    - 부모의 함수로 새로운 객체를 생성
+    - 자식함수의 prototype을 부모 객체로 설정
+    - 이 방법으로 할 경우 자식의 constructor가 깨진다.
+- Object.create 생성자
+    - 프로퍼티 초기화가 번거롭다.
+- new와 Object.create의 결합
+    - protype을 Object.create()로 설정
+    - constructor를 다시 살려놓는다.
+
+- 위임형 상속
+    - 
+- 연결형 상속
+    - Object.assign을 통해 구현함
+    - JQuery, lodash등 .extend()와 비슷
+- 함수형 상속
+    - 새 속성들을 연결형 상속으로 쌓되 상속 기능을 Factory 함수로 만들어 사용함
+    - 객체 확장에 함수를 사용하는 큰 이점은 Private Data를 클로저를 통해 캡슐화 시킬수 있음
---
+++ prototype/index.js
@@ -0,0 +1,106 @@
+function Person() { };
+
+Person.prototype.hand = 2;
+Person.prototype.body = 1;
+Person.prototype.nose = 1;
+
+const kim = new Person();
+const park = new Person();
+
+console.dir(kim);
+console.dir(park);
+
+console.dir(Person);
+
+/* 프로토 타입 */
+/* 프로토 타입 체이닝 */
+/* 프로토 타입 상속 */
+
+// new 생성자를 사용한 상속
+(function () {
+    function Parent() { };
+    function Child() { };
+
+    Child.prototype = new Parent();
+
+    const child = new Child();
+    console.dir(Child);
+    console.dir(child);
+
+    // Child는 체이닝을 통해 Parent의 constructor를 반환
+    // Child의 constructor가 깨짐
+    console.log(Child.constructor === Parent.constructor);
+})();
+
+// Object.create를 사용한 상속
+(function () {
+    function Parent(name) {
+        this.name = name;
+    };
+
+    const child = Object.create(Parent.prototype); // 상속을 했지만 name이 없음
+    // Parent.call(child, 'child'); // name을 초기화, 객체 프로퍼티 설정
+    console.dir(child);
+})();
+
+// new + Object.create()를 이용한 상속
+(function () {
+    function Parent(name) {
+        this.name = name;
+    }
+    function Child(name) {
+        Parent.call(this, name);
+    }
+
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+
+    const child = new Child('child');
+    console.dir(child.name);
+})();
+
+// 위임형 상속
+
+
+
+// 연결형 상속
+(function () {
+    const proto = {
+        hello: function hello() {
+            return `Hello, my name is ${this.name}`;
+        }
+    };
+
+    const george = Object.assign({}, proto, { name: 'George' });
+    const msg = george.hello();
+    console.log(msg); // Hello, my name is George
+})();
+
+// 함수형 상속
+
+(function () {
+    function Person() {}
+
+    Person.prototype.say = function() {
+        console.log(`Hello! My name is ${this.name}`);
+    }
+
+    const rawMixin = function () {
+        const attrs = {};
+        return Object.assign(this, {
+            set(name, value) {
+                attrs[name] = value;
+            },
+            get(name) {
+                return attrs[name];
+            }
+        }, Person.prototype);
+    };
+
+    const mixinModel = (target) => rawMixin.call(target);
+    const george = { name: 'george' };
+    const model = mixinModel(george);
+    model.say();
+    console.dir(model);
+    console.dir(model.__proto__ === Person.prototype);
+})();
